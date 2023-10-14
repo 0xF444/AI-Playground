e887e5066286d8a11cde4e4282a628651f490915
Omar Mokhtar <omar.shatat95@gmail.com>
2023-10-14 23:58:29 +0300

    First ProgramFinished

---
+++ Program1.py
@@ -0,0 +1,4 @@
+units = int(input("Please enter units: "))
+tens = int(input("Please enter tens: "))
+hundereds = int(input("Please enter hundereds: "))
+print(f"The number is {units+tens*10+hundereds*100}.")
